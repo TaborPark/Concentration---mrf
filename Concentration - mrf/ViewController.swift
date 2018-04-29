92d5841170feedd62d322160723046f8dd8adc36
Mark Foege <mfoege@colvosgroup.com>
2018-04-29 16:28:15 -0700

    Initial Commit

---
+++ Concentration - mrf/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Concentration - mrf
+//
+//  Created by Mark Foege on 4/29/18.
+//  Copyright © 2018 Inobus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
